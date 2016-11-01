61deb60fa88a6cabc7686901126d2095808dcfbc
Sabrina Robinson <srobinson.sh@mysummitps.org>
2016-11-01 21:06:23 +0000

    update

---
+++ changer.js
@@ -0,0 +1,8 @@
+//Creates Variables and saves DOM  Elements to each 
+var newBGColor= document.getElementById("newBGColor")
+var colorChangeButton= document.getElementById("colorChangeButton")
+
+
+colorChangeButton.addEventListener("click", function(){
+document.body.style.backgroundColor = newBGColor.value;
+})
